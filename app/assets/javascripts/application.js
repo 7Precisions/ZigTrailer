c3e47ad6e434108c8a6bf9fb03270531d6cf0e74
sonikamaliya <sonikamaliya@gmail.com>
2016-05-05 08:40:27 +0530

    ZigTrailer | Watch, share & Download New Trailer Videos, implemented theme and home page

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,23 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require "01/js/jquery-1.7.min.js"
+//= require "01/js/jquery.easing.1.3.js"
+//= require "01/js/prettyPhoto/js/jquery.prettyPhoto.js"
+//= require "01/js/jquery.quicksand.js"
+//= require "01/js/jquery.validate.min.js"
+//= require "01/js/jquery.history.js"
+//= require "01/js/main.js"
+//= require "01/js/custom.js"
+//= require "01/js/froogaloop.js"
---
+++ app/assets/themes/01/js/custom.js
@@ -0,0 +1,17 @@
+var bgTime = 5000; // bg Image/Video animation display duration
+var bgPaused=false; // bg Image/Video animation paused
+var menuTime = 600; // menu delay
+var autoPlay = false; // Background audio autoplay
+var loop = true; // Background audio loop or next song
+var twtTime = 3000; // Twitter display duration
+var prettyTheme = 'dark_square'; //Pretty Photo Plug-in Teheme
+
+var normalFade = true; // Normal fade animation
+var frontPage = ''; // Front Page URL
+var btnSoundURL = 'http://files.7precisions.com/sounds/button2'; // Button Hover Sound
+var menuPositionFixed = false;
+
+//v1.2
+var videoPaused = false;
+var menuAlwaysOpen = false;
+var bgStretch = true;
